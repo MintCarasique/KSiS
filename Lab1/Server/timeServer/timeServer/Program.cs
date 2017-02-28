fd0f6bc5b56070fa4235323046e3a9e554d5a2a5
MintCarasique <mykew98@gmail.com>
2017-02-28 11:32:22 +0300

    Server part added

---
+++ Lab1/Server/timeServer/timeServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace timeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket currentSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            currentSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+
+            IPEndPoint endP = new IPEndPoint(IPAddress.Broadcast, 12209);
+            currentSocket.Connect(endP);
+
+            Console.WriteLine("Server is working!");
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                string curTime = DateTime.Now.ToLongTimeString();
+                byte[] sendbuf = Encoding.ASCII.GetBytes(curTime);
+                currentSocket.Send(sendbuf);
+            }
+        }
+    }
+}
