278d13538ec4873baeee9204cd192674c2d3d254
MintCarasique <mykew98@gmail.com>
2017-02-28 11:25:56 +0300

    Initial commit

---
+++ Lab1/Client/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace timeClient
+{
+    class Program
+    {
+        private const int listenPort = 12209;
+
+        public static void StartListen()
+        {
+            bool done = false;
+            var listener = new UdpClient(listenPort);
+            var groupEndPoint = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (!done)
+                {
+                    //Console.WriteLine("I'm client.");
+                    byte[] bytes = listener.Receive(ref groupEndPoint);
+                    Console.Clear();
+                    Console.WriteLine("Current time by server: {0} ", Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            StartListen();
+
+        }
+    }
+}
