7cd8d65d083f00731f940ae8a75a42a2a2868a1c
MintCarasique <mykew98@gmail.com>
2017-03-13 20:16:37 +0300

    Initial Commit

---
+++ Lab2/Client/reverseClient/reverseClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseClient
+{
+    class Program
+    {
+        public static void sendMessage()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
