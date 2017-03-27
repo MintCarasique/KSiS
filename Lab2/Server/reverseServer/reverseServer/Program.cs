f70e07266ec40c371c5b4d24dfaffa5cd2599ce4
MintCarasique <mykew98@gmail.com>
2017-03-27 19:47:55 +0300

    Added reverse function

--- Lab2/Server/reverseServer/reverseServer/Program.cs
+++ Lab2/Server/reverseServer/reverseServer/Program.cs
@@ -14,7 +14,8 @@ namespace reverseServer
         private int listenPort = 50255;
         static void Main(string[] args)
         {
-                        
+            Program program = new Program();
+            program.startListen();           
         }
         public void startListen()
         {
@@ -23,6 +24,33 @@ namespace reverseServer
             socket.Bind(EndPoint);
             socket.Listen(10);
             Console.WriteLine("Server started!");
+            while (true)
+            {
+                ProcessingClientData();
+            }
+        }
+        public void ProcessingClientData()
+        {
+            Socket handler = socket.Accept();
+            byte[] buffer = new byte[1024];
+            int byterec = handler.Receive(buffer);
+            string str;
+            str = StringReverse(Encoding.UTF8.GetString(buffer, 0, byterec));
+            handler.Send(Encoding.UTF8.GetBytes(str));
+            if (Encoding.UTF8.GetString(buffer, 0, byterec).IndexOf("<TheEnd>") > -1)
+            {
+                Console.WriteLine("MessageProcessed");
+            }
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+        public string StringReverse(string str)
+        {
+            string message = str.ToString();
+            string[] substrings = message.Split(' ');
+            Array.Reverse(substrings);
+            message = String.Join(" ", substrings);
+            return message;
         }
     }
 }
